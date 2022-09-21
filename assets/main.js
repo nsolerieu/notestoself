5f6068abd4ef4f5efeaec949eae41e729dff9bfb
Nicolas Solerieu <nsolerieu@users.noreply.github.com>
2022-09-20 17:08:02 -0700

    Mostly design
    - Added icon
    - Used same styles as main page
    - fixed up formatting

--- assets/main.js
+++ assets/main.js
@@ -4,15 +4,28 @@ $(document).ready(function() {
 
     $.each( data.notes, function( i, note ) {
       $('.feed').append(
-        '<div class="one-quote-box quote-' +  data.notes[i].id + '">' +
-            '<p class="quote-id">' + data.notes[i].id + '</p>' +
-            '<h2 class="quote-text">' + data.notes[i].quote + '</h2>' +
-            '<p class="quote-author">&mdash;' + data.notes[i].author + '</p>' +
-            '<p class="date">' + data.notes[i].date + '</p>' +
-            '<p class="notes">' + data.notes[i].notes + '</p>' +
-        '</div>'
+        '<div class="one-quote-box quote-' +  data.notes[i].id + '"><div class="one-quote-box__content">' +
+            '<p class="quote-text text-color-primary">' + data.notes[i].quote + '</p>' +
+            '<p class="quote-author text-color-primary">&mdash;' + data.notes[i].author + '</p>' +
+            '<div class="quote-date text-small">' + data.notes[i].date + '</div> &bull; ' +
+            '<div class="view-notes text-small">notes</div>' +
+            '<div class="notes" style="display:none">' + data.notes[i].notes + '</div>' +
+        '</div></div>'
       );
 
     });
   });
+});
+
+
+$('.view-notes').on('click', function() {
+    $(this).next().toggle();
+    $(this).toggleClass( "active" );
+});
+
+$( ".meta-box__button" ).click(function() {
+
+  $(this).toggleClass( "active" );
+  $("body").toggleClass( "light-mode" );
+
 });
