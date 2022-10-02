fc20b6b7bd035fcb9bad8970906f73908fc27874
Nicolas Solerieu <nsolerieu@github.com>
2022-10-02 07:09:34 -0700

    Revamp
    - removed dark mode
    - simplified css
    - changed grid
    - update gutter and margins

--- assets/main.js
+++ assets/main.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
       $('.feed').append(
         '<div class="row one-quote-box quote-' +  data.notes[i].id + '">' +
           '<div class="col-50">' +
-            '<div class="quote-date">' + data.notes[i].id + ' / ' + data.notes[i].date + '</div>' +
+            '<div class="quote-date text-small text-color-secondary">' + data.notes[i].id + ' — ' + data.notes[i].date + '</div>' +
           '</div><div class="col-50">' +
             '<p class="quote-text text-color-primary text-large"> &ldquo;' + data.notes[i].quote + '&rdquo;</p>' +
-            '<p class="quote-author text-regular">&mdash;' + data.notes[i].author + '</p>' +
-            '<div class="quote-notes text-regular">' + data.notes[i].notes + '</div>' +
+            '<p class="quote-author text-small">&mdash; ' + data.notes[i].author + '</p>' +
+            '<div class="quote-notes text-regular text-color-secondary">' + data.notes[i].notes + '</div>' +
         '</div></div>'
       );
 
